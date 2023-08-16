40f22e614f09fb0f58de618bd8db75c610c5eccc
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-16 18:43:49 +0100

    create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Home} from "./pages/home"
+import {Auth} from "./pages/auth"
+import {Create} from "./pages/create"
+import {Saved} from "./pages/saved"
+import {NavBar} from "./components/navbar"
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+	<div className="App">
+		<Router>
+			<NavBar/>
+			<Routes>
+				<Route path='/' element={<Home/>} />
+				<Route path='/auth' element={<Auth/>} />
+				<Route path='/create' element={<Create/>} />
+				<Route path='/saved' element={<Saved/>} />
+			</Routes>
+		</Router>
+	</div>
   );
 }
 
