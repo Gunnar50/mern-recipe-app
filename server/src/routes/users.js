52725e3f18e7268d7970860fffd429a52d1576d4
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-17 17:58:49 +0100

    create verify user route

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -39,6 +39,19 @@ router.post("/login", async (req, res) => {
 
 });
 
+router.get("/verify-token", (req, res) => {
+    const token = req.headers['authorization'];
+    
+    if (!token) return res.status(401).json({ message: "No token provided." });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(500).json({ message: "Failed to authenticate token." });
+        
+        // Token is valid
+        res.json({ valid: true });
+    });
+});
+
 
 
 export {router as userRouter}
