337d413de9592b9b71d94d896694b48f1bdfaddb
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-17 17:58:31 +0100

    create verifyusers

---
+++ client/src/utils/verifyUserToken.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const verifyUserToken = async (token) => {
+    try {
+        const response = await axios.get('http://localhost:3001/auth/verify-token', {
+            headers: {
+                'Authorization': token
+            }
+        });
+        return response.data.valid;
+    } catch (error) {
+        return false;
+    }
+};
