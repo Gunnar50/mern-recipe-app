3acff23f2ca8cc6803f520694b3fd6548745ac43
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-17 19:22:12 +0100

    updated protect routes

--- client/src/routes/ProtectedRoute.js
+++ client/src/routes/ProtectedRoute.js
@@ -1,20 +1,29 @@
 import { useNavigate } from 'react-router-dom';
 import { verifyUserToken } from '../utils/verifyUserToken';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-    const isVerified = verifyUserToken(localStorage.getItem('access_token'));
+    const [isVerified, setIsVerified] = useState(null);
     console.log(isVerified);
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (!isVerified) {
-            navigate("/");
-        }
-    }, [isVerified, navigate]);
+        // this will verify the token and then update our state
+        verifyUserToken(localStorage.getItem('access_token'))
+            .then(result => {
+                setIsVerified(result);
+            })
+            .catch(error => {
+                console.error("Error verifying token:", error);
+                setIsVerified(false);
+            });
+    }, []);  // empty dependency array means this effect will only run once, when the component mounts
+
+
+    if (isVerified === null) return null;
 
     return (
-        isVerified ? <Component {...rest} /> : null
+        isVerified ? <Component {...rest} /> : navigate("/auth")
     );
 };
 
