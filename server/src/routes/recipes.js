986e221326ba13fe0387c7ede1b50a6d99b56317
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-18 16:09:35 +0100

    added the routes for recipes

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,9 +1,12 @@
-import express from "express.js";
+import express from "express";
 import mongoose from "mongoose";
 import { RecipeModel } from "../models/Recipes.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import { getAllRecipes, createRecipe } from "../controllers/recipesController.js";
 
 const router = express.Router();
 
-
+router.get("/", getAllRecipes)
+router.post("/create", createRecipe)
 
 export {router as recipesRouter};
