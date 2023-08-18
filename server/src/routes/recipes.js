4b8d78e195a7227ac101428e4ae9d629050c070b
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-18 11:41:13 +0100

    created recipe route

---
+++ server/src/routes/recipes.js
@@ -0,0 +1,8 @@
+import express from "express.js";
+import mongoose from "mongoose";
+import { RecipeModel } from "../models/Recipes.js";
+
+const router = express.Router();
+
+
+export {router as recipesRouter};
