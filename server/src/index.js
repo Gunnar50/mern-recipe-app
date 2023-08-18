90c592cb79ca5af79b6fde21dc95c26ece9f08ef
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-18 11:40:37 +0100

    add router for recipes

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,14 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
 
 app.use(express.json());  // when sending data from the frontend it will convert into json automatically
 app.use(cors());
 app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
 mongoose.connect(process.env.MONGO_URI);
 
