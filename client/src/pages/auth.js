506d643d8ba08a074987cde67555e745ebae8f00
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-18 09:50:23 +0100

    updated onsubmit

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -16,21 +16,49 @@ const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
-
     const [, setCookies] = useCookies(["access_token"])
+
+    // const onSubmit = async (event) => {
+    //     event.preventDefault();
+    //     try{
+    //         const response = await axios.post("http://localhost:3001/auth/login", {username, password});
+            
+    //         setCookies("access_token", response.data.token);
+    //         window.localStorage.setItem("userID", response.data.userID);
+    //         navigate("/");
+    //         console.log(response);
+
+    //     } catch(err) {console.error(err)};
+    // }
 
     const onSubmit = async (event) => {
         event.preventDefault();
-        try{
-            const response = await axios.post("http://localhost:3001/auth/login", {username, password});
+        try {
+            const response = await axios.post("http://localhost:3001/auth/login", { username, password });
             
-            setCookies("access_token", response.data.token);
-            window.localStorage.setItem("userID", response.data.userID);
-            navigate("/");
-            console.log(response);
+            const token = response.data.token;
+            const userID = response.data.userID;
 
-        } catch(err) {console.error(err)};
+            // Immediately verify token after successful login
+            const verifyResponse = await axios.get("http://localhost:3001/auth/verify-token", {
+                headers: { 'Authorization': token }
+            });
+
+            if (!verifyResponse.data.valid) {
+                console.log('Token verification failed!');
+                return;
+            }
+
+            setCookies("access_token", token);
+            window.localStorage.setItem("userID", userID);
+            navigate("/");
+
+        } catch (err) {
+            console.error(err);
+            alert(err.response ? err.response.data.message : "Error occurred during login.");
+        }
     }
+
 
     return (
         <div className="col-md-5 card card-body mr-md-3 p-5">
