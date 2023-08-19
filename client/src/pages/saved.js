67e015c6845cc1f8e6dd7182d68ff354d2a38ca4
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-19 17:55:21 +0100

    created saved/favourite page

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -1,7 +1,59 @@
-
-
-
+import { useEffect, useState } from "react"
+import axios from "axios";
+import {useCookies} from "react-cookie"
 
 export const Saved = () => {
-    return <div>Saved</div>
+    const [votedRecipes, setVotedRecipes] = useState([]);
+    const [currentUsername, setCurrentUsername] = useState("");
+    const [cookies, ] = useCookies(["access_token"])
+    const userID = window.localStorage.getItem("userID");
+
+
+    useEffect(() => {
+        const fetchVotedRecipes = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3001/recipes/get-voted-recipes/${userID}`);
+                setVotedRecipes(response.data.votedRecipes);
+                
+            } catch (err) {console.error(err);}
+        }
+        
+        const getCurrentUser = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3001/auth/user/${userID}`);
+                setCurrentUsername(response.data.username);
+                
+            } catch (err) {console.error(err);}
+        }
+
+        getCurrentUser();
+        if(cookies.access_token) fetchVotedRecipes();
+    }, [])
+
+    
+
+    return (
+        <div className="row justify-content-center mt-4">
+            {votedRecipes.map((item, idx) => (
+                <div key={item._id} className="col-md-10 card card-body p-2 mb-2">
+                    <div className="d-flex justify-content-between align-items-center">
+                        {/* Image on the left side */}
+                        <div>
+                            <img src={item.image} alt="Recipe" className="img-fluid" style={{ maxWidth: '150px' }} />
+                        </div>
+
+                        {/* Middle content with title, description, and view link */}
+                        <div style={{ flex: 2, margin: '0 20px' }}>
+                            <h2 className="card-title">{item.name}</h2>
+                            <p>Cooking Time: {item.cookingTime} minutes</p>
+                            <h6>Author: {item.creator.username ? item.creator.username == currentUsername ? `${item.creator.username} (Me)` : item.creator.username : 'Unknown'}</h6>
+                            <a href="/your-recipe-link">View Recipe</a>
+                        </div>
+
+                        
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
 }
