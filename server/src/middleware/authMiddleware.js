05ec54cc7e2a9fa0f6378779e2a7d06992616025
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-29 14:32:08 +0100

    updated middleware

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const authMiddleware = (req, res, next) => {
-    const token = req.headers['authorization'];
+    const token = req.headers.authorization;
 
-    if (!token) return res.status(401).json({ message: "No token provided." });
+    if (!token) return res.status(401).json({ message: "No token provided.", valid: false });
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) return res.status(500).json({ message: "Failed to authenticate token." });
+        if (err) return res.status(403).json({ message: "Failed to authenticate token.", valid: false});
 
         req.userId = decoded.id;  // store the user id in request for future use
         next();
