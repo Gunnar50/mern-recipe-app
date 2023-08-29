d99c7c0d026d3886ee84e9c3f8bf94b17e63bf69
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-29 14:31:58 +0100

    created verify token separete function

---
+++ client/src/pages/verify-token.js
@@ -0,0 +1,17 @@
+
+
+export async function verifyToken(token) {
+    try {
+        const verifyResponse = await axios.get("http://localhost:3001/auth/verify-token", 
+            {
+                headers: { authorization: token }
+            });
+        if (!verifyResponse.data.valid) {
+            console.log("Token verification failed!");
+            return false;
+        }
+        return true;
+    } catch (err) {
+        console.error(err);
+    }
+} 
