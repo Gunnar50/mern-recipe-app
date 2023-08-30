a5722c4bfb84ce6502b0f70693f55f6cb53e783c
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-30 17:24:08 +0100

    refractor user routes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,15 +1,15 @@
 import express from "express";
-import { registerUser, loginUser, verifyToken, getCurrentUser } from "../controllers/userController.js";
-import {authMiddleware} from "../middleware/authMiddleware.js";
+import { getCurrentUser, loginUser, registerUser, verifyToken } from "../controllers/userController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/verify-token", authMiddleware, verifyToken); // applied the middleware before the verifyToken controller
+// router.get("/verify-token", authMiddleware, verifyToken); // applied the middleware before the verifyToken controller
 router.get("/user/:id", getCurrentUser);
 
-export {router as userRouter}
+export { router as userRouter };
 
 
