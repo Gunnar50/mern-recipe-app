3a4200801049c9bf9410bc90241f70feeaf751b6
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-30 17:28:47 +0100

    add middleware for authentication

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,6 +1,6 @@
 import express from "express";
+import { createComment, createRecipe, deleteRecipe, getAllRecipes, getOwnRecipes, getRecipeID, getVotedRecipes, getVotedRecipesIDs, updateRecipe, voteForRecipe } from "../controllers/recipesController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { getAllRecipes, createRecipe, voteForRecipe, getVotedRecipes, getVotedRecipesIDs, getRecipeID, getOwnRecipes, deleteRecipe, updateRecipe, createComment } from "../controllers/recipesController.js";
 
 const router = express.Router();
 
@@ -8,11 +8,11 @@ router.get("/", getAllRecipes)
 router.get("/get-recipes/:id", getVotedRecipesIDs)
 router.get("/get-voted-recipes/:id", getVotedRecipes)
 router.get("/get-recipe/:recipeid", getRecipeID)
-router.get("/:userid", getOwnRecipes)
-router.put("/", voteForRecipe)
-router.put("/update/:recipeid", updateRecipe)
-router.post("/create", createRecipe)
-router.post("/:recipeid", createComment)
-router.delete("/del/:id", deleteRecipe)
+router.get("/:userid", authMiddleware, getOwnRecipes)
+router.put("/", authMiddleware, voteForRecipe)
+router.put("/update/:recipeid", authMiddleware, updateRecipe)
+router.post("/create", authMiddleware, createRecipe)
+router.post("/:recipeid", authMiddleware, createComment)
+router.delete("/del/:id", authMiddleware, deleteRecipe)
 
-export {router as recipesRouter};
+export { router as recipesRouter };
